0ecb126fcf7700841b25efb845c5ece44dddeaa4
defaultkavy <defaultkavy@gmail.com>
2026-04-01 00:05:15 +0800

    feat!: new required options member `selector` to find where app insert into

---
+++ src/structure/CheerioProto.ts
@@ -0,0 +1,37 @@
+import type { Proto } from "amateras/core";
+import { load, type Cheerio } from 'cheerio';
+
+export class CheerioProto {
+    $html: Proto;
+    $container: Proto | null = null;
+    constructor(html: string, rootSelector: string, containerSelector: string) {
+        const cheerio$ = load(html);
+        const c_html = cheerio$(rootSelector);
+        this.$html = this.toProto(c_html, containerSelector);
+    }
+
+    toProto(element: Cheerio<any>, containerSelector: string): Proto {
+        const tagname = element.prop('tagName')?.toLowerCase();
+        if (!tagname) throw 'tsukimi parse proto error: tagname is undefined';
+        const attr = element.attr() ?? {};
+        const proto = $(tagname, attr, () => {
+            element.prop('childNodes').forEach(childNode => {
+                switch (childNode.type) {
+                    case 'text': {
+                        const text = childNode.data.trim();
+                        if (text.length) $([ text ]);
+                        break;
+                    }
+                    case 'script':
+                    case 'tag': {
+                        this.toProto(element.children(childNode), containerSelector);
+                        break;
+                    }
+                }
+            })
+        });
+        console.debug(element.is(containerSelector), element.prop('tagName'))
+        if (!this.$container && element.is(containerSelector)) this.$container = proto;
+        return proto;
+    }
+}
---
+++ src/structure/Tsukimi.ts
@@ -0,0 +1,113 @@
+import 'amateras';
+import { ElementProto, Proto } from 'amateras/core';
+import { _instanceof, forEach } from 'amateras/utils';
+import { CheerioProto } from './CheerioProto';
+
+export interface TsukimiConfig {
+    entrypoint?: string;
+    app: Proto;
+    selector: string;
+    outDir?: string;
+}
+
+export class Tsukimi {
+    outDir: string;
+    app: Proto;
+    selector: string;
+    constructor(config: TsukimiConfig) {
+        this.outDir = config.outDir ?? 'dist';
+        this.app = config.app;
+        this.selector = config.selector;
+        this.bundler(config.entrypoint);
+    }
+
+    async render(path: string) {
+        const index_html = await Bun.file(this.outDir + '/index.html').text();
+        const result = new CheerioProto(index_html, 'html', this.selector);
+        const {$html} = result;
+        $html.build();
+        if (!result.$container) throw 'Tsukimi.render(): container element not found';
+        const $head = $html.findBelow(proto => _instanceof(proto, ElementProto) && proto.tagname === 'head')
+        // assign app global to $head
+        $.context(Proto, result.$container, () => {
+            const $app = $(this.app);
+            result.$container?.append($app);
+            $app.build();
+        })
+        //@ts-ignore
+        if ($html.global.router) await Promise.all($html.global.router.resolve(path));
+        
+        // await app promises
+        async function awaitPromises() {
+            const promises = Array.from($html.global.promises);
+            await Promise.all(promises);
+            if ($html.global.promises.size) await awaitPromises();
+        }
+
+        await awaitPromises();
+        if ($head) {
+            // assign children global to $head
+            $.context(Proto, $head, () => {
+                let cssText = ''
+                forEach($.styleMap, ([constructor, css]) => {
+                    if ($html.findBelow(proto => proto.constructor === constructor)) forEach(css, rule => cssText += rule);
+                })
+                //@ts-ignore
+                if ($.CSS) {
+                    //@ts-ignore
+                    cssText += $.CSS.text($html);
+                }
+                if (cssText.length) {
+                    const $style = $('style', {id: '__ssr__'}, () => $([ cssText ]));
+                    $head.append($style);
+                    $style.build();
+                }
+                //@ts-ignore
+                if ($html.global.prefetch) {
+                    //@ts-ignore
+                    const $script = $('script', () => $`window.prefetch = ${JSON.stringify($html.global.prefetch.caches)}`);
+                    $head.append($script);
+                    $script.build();
+                }
+                //@ts-ignore
+                if ($html.global.title) {
+                    //@ts-ignore
+                    const $title = $('title', () => $([$html.global.title]))
+                    $head.append($title);
+                    $title.build();
+                }
+                //@ts-ignore
+                if ($html.global.meta) {
+                    //@ts-ignore
+                    let metaList: [] = $.meta.resolve($html.global.meta);
+                    forEach(metaList, (meta => {
+                        const $meta = $('meta', meta);
+                        $head.append($meta);
+                        $meta.build();
+                    }))
+                }
+            })
+        }
+        const html = $html.toString();
+        $html.dispose();
+        return html;
+    }
+
+
+    private async bundler(entrypoint?: string) {
+        const worker = new Worker(`${__dirname}/../lib/bundlerWorker.ts`);
+        worker.onmessage = (e) => {
+            if (e.data === 'ready') {
+                worker.postMessage({outDir: this.outDir, entrypoint});
+            }
+            else {
+                // this.#bundlerOutput = e.data;
+                worker.terminate();
+            }
+        }
+
+        worker.onerror = err => {
+            console.error(err.message)
+        }
+    }
+}
